a9d556ea0ae3a3c3a2e5c23d4881a29722bc0488
mich2k <inbox@gmichele.com>
2024-03-19 19:15:41 +0100

    up

--- ext/SimCLRv2-Pytorch/exec.sh
+++ ext/SimCLRv2-Pytorch/exec.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python3 verify.py r50_1x_sk0.pth ILSVRC2012_img_val/ 380
+python3 verify.py -pth_path r101_1x_sk0.pth -val_path ILSVRC2012_img_val/ -n_samples 2 -batch_size 32
--- ext/SimCLRv2-Pytorch/verify.py
+++ ext/SimCLRv2-Pytorch/verify.py
@@ -17,7 +17,7 @@ class ImagenetValidationDataset(Dataset):
         self.val_path = val_path
         self.labels = []
         self.transform = transforms.Compose([transforms.Resize(256), transforms.CenterCrop(224), transforms.ToTensor()])
-        with open('/home/mich/Scrivania/WSI-DeepMIML-Framework/ext/SimCLRv2-Pytorch/ILSVRC2012_img_val/val.txt') as f:
+        with open(val_path + "val.txt") as f:
             self.labels = [int(l.strip().split(' ')[1]) - 1 for l in f.readlines()]
 
     def __len__(self):
@@ -32,12 +32,15 @@ def accuracy(output, target, topk=(1,)):
     maxk = max(topk)
     _, pred = output.topk(maxk, 1, True, True)
     pred = pred.t().cpu()
-    correct = pred.eq(target.view(1, -1).expand_as(pred))
+    correct = pred.eq(target[0].view(1, -1).expand_as(pred))
     res = []
     for k in topk:
-        correct_k = correct[:k].view(-1).float().sum().item()
+        correct_k = correct[:k].view(-1).float().sum().item() # correct_k.view(-1).nonzero()
+        # in poche parole effettivamente ce ne sono di corrette
+        # MA, non sono altro che quelle in cui la classe target è proprio 0
         res.append(correct_k)
-    return res
+    how_many_had_zero = (target[0].shape[0] - target[0].nonzero().shape[0])
+    return res, how_many_had_zero
 
 
 @torch.no_grad()
@@ -53,17 +56,18 @@ def run(pth_path, val_path, n_samples, batch=380): # default for debug
     
     zero=0
     counter=0
+    
     for images, labels in tqdm(data_loader):
-        #print(images)
+
         if counter == n_samples and n_samples != 0: # since dataloader loads/samples batch_size at time we can early stop (easiest approach)
-            pass
+            break
         _, pred = model(images.to(device), apply_fc=True).topk(1, dim=1)    # pred shape: (380, 1), (batch size, 1)
         
         preds.append(pred.squeeze(1).cpu())
         #print(pred)
-        #predcpu= pred.squeeze(1).cpu().numpy()
+
         if(pred.sum() == 0):    # dim=None reduces all dimensions
-            print(f"pred is zero @{zero}")
+            print(f"pred tensor is zero @ {zero}")
             zero+=1
         
         target.append(labels)
@@ -72,32 +76,64 @@ def run(pth_path, val_path, n_samples, batch=380): # default for debug
     #print(p, t)    # throws error! UnboundLocalError: cannot access local variable 'p' where it is not associated with a value
     p = torch.cat(preds).numpy()
     t = torch.cat(target).numpy()
+    
+    
+    # I make 1000 counters, one for each class
+    # I index at the t[i] counter, i-th is the image, meanwhile t[i] is its class
+    # all_counters[t[i]] hence is the Counter for that specific class
+    
+    # I then increment the counter at the p[i] index, which is the prediction for the i-th image
+    # if [t[i]] is the correct class, lets say 65, I want that p[i], the prediction for the i-th image, to be 65 too
+    # all_counters[65][65] -> we want this ideally to match for all the images which are in the class 65
+    
+    
     all_counters = [Counter() for i in range(1000)]
     for i in range(n_samples*batch):    # now respecting the n_samples
         all_counters[t[i]][p[i]] += 1
     total_correct = 0
     for i in range(1000):
         try:
-            total_correct += all_counters[i].most_common(1)[0][1] # most_common returns a list, if counter is empty then we get index out of range with []
+            # I pick the i-th counter with all_counters[i]
+            # then i retrieve the top-k common predicted classes for that class, in this case the top 1
+            # this returns me a list of the top-k tuples, we have k=1 hence will be a list with one tuple inside, with [0] i retrieve the only tuple in the list, [(0,3)]
+            # now we get our tuple, in the following fashion (0,3), this means for the class i, i predicted 0 three times (hope that i=0)
+            # with the [1] i get the second element of that tuple, hence 3 which is the number of predictions for the most common class for the i-th class
+            
+            total_correct += all_counters[i].most_common(1)[0][1] 
         except IndexError:
-            print(f"Empty counter at index {i}")
+            #print(f"Empty counter at index {i}") # most_common returns a list, if counter is empty then we get index out of range with []
             pass
+    print(f"Total correct: {total_correct}")
+    
+    
+    real_beccati, how_many_targets_had_class_zero = accuracy(pred, target)
+    real_beccati = int(real_beccati[0])
+    
+    print('\n----------------------------------------')
+    print(f"Beccati {real_beccati} su {n_samples*batch}")
+    print(f"Amongst these {how_many_targets_had_class_zero} had class zero as a target")
+    
+    print(f"Accuracy real: {str(real_beccati/(n_samples*batch)*100)[:5]}")
+    
     print(f'ACC: {total_correct / (n_samples*batch) * 100}')
+    print('----------------------------------------\n')
+
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='SimCLR verifier')
-    parser.add_argument('-pth_path', default='ext/SimCLRv2-Pytorch/r50_1x_sk0.pth', type=str, help='path of the input checkpoint file')
-    parser.add_argument('-val_path', type=str, help='path of the validation dataset', default="ext/SimCLRv2-Pytorch/ILSVRC2012_img_val/")
+    parser.add_argument('-pth_path', default='ext/SimCLRv2-Pytorch/r101_1x_sk0.pth', type=str, help='path of the input checkpoint file')
+    parser.add_argument('-val_path',  default="ext/SimCLRv2-Pytorch/ILSVRC2012_img_val/", type=str, help='path of the validation dataset')
     
     # n_samples = 0 takes the whole dataloader, as batch_size*n_sample,
     #   for instance being our dataset 50k it will try with 50000/380=131.579
     #   so it will try with 132 batches, thus reaching 132*380=50160 samples
      
-    parser.add_argument('-n_samples', default=3, type=int, help='number of batch_size samples load')
+    parser.add_argument('-n_samples', default=1, type=int, help='number of batch_size samples load')
     parser.add_argument('-batch_size', default=380, type=int, help='batch size')
 
     args = parser.parse_args()
+    print('\n----------------------------------------')
     
     # fix:
     # https://stackoverflow.com/questions/66857471/cuda-initialization-cuda-unknown-error-this-may-be-due-to-an-incorrectly-set
@@ -110,6 +146,13 @@ if __name__ == '__main__':
         exit(1)  
         
     print(f"using batch size of: {args.batch_size}")
-    print(f"sample size of: {args.n_samples}")
+    
+    if(args.n_samples == 0):
+        print(f"using whole dataset")
+    else:
+        print(f"sample size of: {args.n_samples}")
+    
+    print('----------------------------------------\n')
+
 
     run(args.pth_path, args.val_path, args.n_samples, args.batch_size)
